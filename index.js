b4925786a70ed122be616558778595d5ddcf25e9
lunarera <218800429+lunarera@users.noreply.github.com>
2025-07-24 19:08:43 +0000

    delete muh code

--- index.js
+++ index.js
@@ -24,14 +24,7 @@ import express from 'express'
 const app = express()
 const PORT = 8908
 
-app.use(async(req,res,next)=>{
-    let extReq = await fetch(websiteRootUrl+req.url)
-    console.log(extReq.headers)
-    res.set("content-type", extReq.headers.get("content-type"))
-    res.status(extReq.status)
-    res.send((await extReq.text()))
-})
 
 app.listen(PORT, ()=>{
-    console.log("Please visit port: "+PORT+" This will scrape the site for needed assets")
+    console.log("Visit port "+PORT+" and continue the setup")
 })
