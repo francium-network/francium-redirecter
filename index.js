ddc2dff5814f0a90f4f5f8185964d669632f7ad8
lunarera <218800429+lunarera@users.noreply.github.com>
2025-07-24 19:04:46 +0000

    death to me

--- index.js
+++ index.js
@@ -17,3 +17,21 @@ async function getInput(question){
 }
 
 const websiteURL = await getInput("What website do you want to clone?\n")
+let websiteRootUrl = new URL(websiteURL).origin
+
+import express from 'express'
+
+const app = express()
+const PORT = 8908
+
+app.use(async(req,res,next)=>{
+    let extReq = await fetch(websiteRootUrl+req.path)
+    console.log(extReq.headers)
+    res.set("content-type", extReq.headers.get("content-type"))
+    res.status(extReq.status)
+    res.send((await extReq.text()))
+})
+
+app.listen(PORT, ()=>{
+    console.log("Please visit port: "+PORT+" This will scrape the site for needed assets")
+})
